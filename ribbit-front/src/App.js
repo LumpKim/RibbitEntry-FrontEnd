a8ae7ba4d3d002ce2459e5d15675f69c7c931b8b
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-11-26 22:18:20 +0900

    ADD : 리빗 App 파일

---
+++ ribbit-front/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Header } from './components/index';
+import { Main } from './container/index';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <React.Fragment>
+            <Header />
+              <Switch>
+
+                {/* 메인 페이지*/}
+                <Route path='/' component={Main} exact/>
+
+                {/*  */}
+                
+              </Switch>
+          </React.Fragment>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
