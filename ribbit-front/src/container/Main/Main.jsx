760f43077c64606e1770c6d109b8b53c3503fd4a
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-11-26 22:23:22 +0900

    ADD : 리빗 메인페이지

---
+++ ribbit-front/src/container/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        a
+      </div>
+    );
+  }
+}
+
+export default Main;
