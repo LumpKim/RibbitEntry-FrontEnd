380b6041ae2b023a7426253db0bcc2155d7ba1ec
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-01 23:53:44 +0900

    ADD : photo's index

---
+++ ribbit-front/src/assets/index.js
@@ -0,0 +1,11 @@
+import RibbitIconTranslation from './Ribbit-icon-translation.png';
+import RibbitIcon from './Ribbit-icon.png';
+import RibbitOff from './Ribbit-off.png';
+import TalkingBalloon from './TalkingBalloon.png';
+
+export {
+  RibbitIconTranslation,
+  RibbitIcon,
+  RibbitOff,
+  TalkingBalloon
+}
