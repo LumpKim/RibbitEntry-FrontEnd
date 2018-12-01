edd55d22f30e7bcaccb850e6b30aa20721cadd27
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-02 02:07:21 +0900

    UPDATE : 마이페이지와 유저정보 관람페이지 추가

--- ribbit-front/src/container/index.js
+++ ribbit-front/src/container/index.js
@@ -1,7 +1,15 @@
 import Main from './Main/Main';
+import User from './User/User';
+import Mypage from './MyPage/Mypage';
 
 export {
 
   // 메인페이지
   Main,
+
+  // 유저 정보 페이지
+  User,
+
+  // 마이페이지
+  Mypage,
 }
