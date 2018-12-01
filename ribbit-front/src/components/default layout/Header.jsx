cfb6681e715b522a3de465f25055c3c2cbbe248e
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-02 02:07:35 +0900

    UPDATE : 주석

--- ribbit-front/src/components/default layout/Header.jsx
+++ ribbit-front/src/components/default layout/Header.jsx
@@ -9,7 +9,9 @@ let intervalld = 0;
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      
+    };
   }
 
   // 스크롤을 0.0034초 마다 -50px씩 줄임.
@@ -43,7 +45,7 @@ class Header extends Component {
           </div>
           <div className="filter" />
           <div className="features">
-            <Link to="/user/" className="header__profile">
+            <Link to={`/user/${/* 여기에 nickname | 1은 삭제 -> */'1'}`} className="header__profile">
             
               {/* 이 태그는 이미지를 넣는 기능을 만들면 바뀌도록 해줘야함. - 변동된 엘리멘트의 class명은 이것의 마지막 class와 같음. */}
               <i class="fas fa-user user__profile" />{" "}
