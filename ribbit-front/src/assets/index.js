acf90adf4354e42e51735c6d129e305cb5d16dc0
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-03 19:03:35 +0900

    Revert "."
    This reverts commit 0a533266a2fafceed716682d789c96d2e499e7d4.

--- ribbit-front/src/assets/index.js
+++ ribbit-front/src/assets/index.js
@@ -2,8 +2,10 @@ import RibbitIconTranslation from './Ribbit-icon-translation.png';
 import RibbitIcon from './Ribbit-icon.png';
 import RibbitOff from './Ribbit-off.png';
 import TalkingBalloon from './TalkingBalloon.png';
-import FrogAudio from './Frog.wav';
 
 export {
-  RibbitIconTranslation, RibbitIcon, RibbitOff, TalkingBalloon, FrogAudio,
-};
+  RibbitIconTranslation,
+  RibbitIcon,
+  RibbitOff,
+  TalkingBalloon
+}
--- ribbit-front/src/components/default layout/Header.jsx
+++ ribbit-front/src/components/default layout/Header.jsx
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { RibbitIconTranslation } from '../../assets/index';
-import RibbitContainer from './RibbitContainer';
-import './css/Header.css';
+import React, { Component } from "react";
+import { RibbitIconTranslation } from "../../assets/index";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 // 스크롤 이벤트를 발생시키기 위한 변수
 let intervalld = 0;
@@ -11,7 +10,7 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ribbitTurn: false,
+      
     };
   }
 
@@ -23,55 +22,41 @@ class Header extends Component {
     window.scroll(0, window.pageYOffset - 50);
   };
 
-  // 비동기를 통한 window객체 조종.
+  // 비동기를 통한 window객체 조종
   scrollToTop = async () => {
     intervalld = await setInterval(this.scrollStep, 3.4);
   };
 
-  // 리빗을 클릭시 배경이 나오게 하는 함수.
-  ChangeRibbitToggle = () => {
-    const { ribbitTurn } = this.state;
-    this.setState({
-      ribbitTurn: !ribbitTurn,
-    });
-  };
-
   render() {
-    const { ribbitTurn } = this.state;
     return (
-      <React.Fragment>
-        <div
-          onClick={this.ChangeRibbitToggle}
-          style={ribbitTurn ? { display: 'flex' } : { display: 'none' }}
-          className="ribbit__background"
-        >
-          <RibbitContainer Turn={ribbitTurn} />
-        </div>
-        <div className="topbar">
-          <div className="topbar-content">
-            <Link to="/" className="navigation">
-              <i className="fas fa-home" />
-              <span>홈</span>
+      <div className="topbar">
+        <div className="topbar-content">
+          <Link to="/" className="navigation">
+            <i class="fas fa-home" />
+            <span>홈</span>
+          </Link>
+          <div className="filter" />
+          <div onClick={this.scrollToTop} className="mainLogo">
+            <img
+              className="Logo"
+              src={RibbitIconTranslation}
+              alt="투명한배경의 로고"
+            />
+          </div>
+          <div className="filter" />
+          <div className="features">
+            <Link to={`/user/${/* 여기에 nickname | 1은 삭제 -> */'1'}`} className="header__profile">
+            
+              {/* 이 태그는 이미지를 넣는 기능을 만들면 바뀌도록 해줘야함. - 변동된 엘리멘트의 class명은 이것의 마지막 class와 같음. */}
+              <i class="fas fa-user user__profile" />{" "}
             </Link>
             <div className="filter" />
-            <div onClick={this.scrollToTop} className="mainLogo">
-              <img className="Logo" src={RibbitIconTranslation} alt="투명한배경의 로고" />
-            </div>
-            <div className="filter" />
-            <div className="features">
-              <Link to={`/user/${'1'}`} className="header__profile">
-                {/* 이 태그는 이미지를 넣는 기능을 만들면 바뀌도록 해줘야함. - 변동된 엘리멘트의 class명은 이것의 마지막 class와 같음. */}
-                <i className="fas fa-user user__profile" />
-                {' '}
-              </Link>
-              <div className="filter" />
-              <div onClick={this.ChangeRibbitToggle} className="header__ribbit">
-                <span className="ribbit__text">리빗</span>
-              </div>
+            <div className="header__ribbit">
+              <span className="ribbit__text">리빗</span>
             </div>
           </div>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
