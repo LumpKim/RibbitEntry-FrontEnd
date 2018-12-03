8a1012db1f2ac076d449d4732fc7d6150a39e385
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-03 19:43:23 +0900

    UPDATE : frog sound넣음.

--- ribbit-front/src/assets/index.js
+++ ribbit-front/src/assets/index.js
@@ -2,7 +2,8 @@ import RibbitIconTranslation from './Ribbit-icon-translation.png';
 import RibbitIcon from './Ribbit-icon.png';
 import RibbitOff from './Ribbit-off.png';
 import TalkingBalloon from './TalkingBalloon.png';
+import FrogAudio from './frog.mp3';
 
 export {
-  RibbitIconTranslation, RibbitIcon, RibbitOff, TalkingBalloon,
+  RibbitIconTranslation, RibbitIcon, RibbitOff, TalkingBalloon, FrogAudio,
 };
