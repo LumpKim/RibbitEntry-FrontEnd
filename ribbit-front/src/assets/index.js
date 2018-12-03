0a533266a2fafceed716682d789c96d2e499e7d4
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-03 19:00:28 +0900

    .

--- ribbit-front/src/assets/index.js
+++ ribbit-front/src/assets/index.js
@@ -2,10 +2,8 @@ import RibbitIconTranslation from './Ribbit-icon-translation.png';
 import RibbitIcon from './Ribbit-icon.png';
 import RibbitOff from './Ribbit-off.png';
 import TalkingBalloon from './TalkingBalloon.png';
+import FrogAudio from './Frog.wav';
 
 export {
-  RibbitIconTranslation,
-  RibbitIcon,
-  RibbitOff,
-  TalkingBalloon
-}
+  RibbitIconTranslation, RibbitIcon, RibbitOff, TalkingBalloon, FrogAudio,
+};
---
+++ ribbit-front/src/components/default layout/RibbitContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class RibbitContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div />
+      </div>
+    );
+  }
+}
+
+export default RibbitContainer;
