37694c87ff2e4fc808b7dd8597951762e239f4ae
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-08 20:48:17 +0900

    UPDATE : 퍼블리싱에 기능 추가

--- ribbit-front/src/components/defaultLayout/CancleAlert.jsx
+++ ribbit-front/src/components/defaultLayout/CancleAlert.jsx
@@ -13,15 +13,16 @@ class CancleAlert extends Component {
   };
 
   render() {
-    const { ribbitCancle, KeepChange } = this.props; // boolean
+    // ribbitCancle = boolean, KeepChange = function, DeleteRibbit = function
+    const { KeepChange, ribbitCancle, DeleteRibbit } = this.props;
     return (
       <div
         className="PopUp__background"
         onClick={KeepChange}
         style={ribbitCancle ? { display: 'flex' } : { display: 'none' }}
       >
-        <div className="PopUp__Container">
-          <div onClick={event => this.MaintainContent(event)} className="RibbitPopup__Head">
+        <div onClick={event => this.MaintainContent(event)} className="PopUp__Container">
+          <div className="RibbitPopup__Head">
             <span className="PopupHead__Title">이 리빗을 삭제할까요?</span>
             <i onClick={KeepChange} className="fas fa-times PopupHead__Close" />
           </div>
@@ -29,10 +30,10 @@ class CancleAlert extends Component {
             <span className="PopupBody__Content">정말 이 리빗을 삭제하시겠습니까?</span>
           </div>
           <div className="RibbitPopup__Foot">
-            <div className="RibbitFoot__Cancle">
+            <div onClick={KeepChange} className="RibbitFoot__Cancle">
               <span className="FootCancle__Title">취소</span>
             </div>
-            <div className="RibbitFoot__Delete">
+            <div onClick={DeleteRibbit} className="RibbitFoot__Delete">
               <span className="FootDelete__Title">삭제</span>
             </div>
           </div>
