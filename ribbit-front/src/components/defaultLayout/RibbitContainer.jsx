bd28ab876871607c1bda3ce41fba72efd11674b5
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-09 08:34:39 +0900

    UPDATE : 이미지 파일을 불러와서 state에 담는 작업을 하는 함수 생성

--- ribbit-front/src/components/defaultLayout/RibbitContainer.jsx
+++ ribbit-front/src/components/defaultLayout/RibbitContainer.jsx
@@ -5,12 +5,57 @@ import CancleAlert from './CancleAlert';
 class RibbitContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      count: 1,
+    };
   }
+
+  // state의 데이터를 base64로 인코딩
+  encodeBase64ImageFile = image => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = e => resolve(e.target.result);
+    reader.onerror = error => reject(Error(error));
+  });
+
+  // 리빗할때 이미지파일을 state에 저장함.
+  handleFileRibbit = async (e) => {
+    const { count } = this.state;
+
+    const fileList = e.target.files;
+    let file = fileList[0];
+
+    const data = await this.encodeBase64ImageFile(file);
+
+    if (count === 1) {
+      this.setState({
+        ribbitImage1: data,
+        count: count + 1,
+      });
+    } else if (count === 2) {
+      this.setState({
+        ribbitImage2: data,
+        count: count + 1,
+      });
+    } else if (count === 3) {
+      this.setState({
+        ribbitImage3: data,
+        count: count + 1,
+      });
+    } else {
+      alert('이미지 한도를 초과하였습니다.');
+    }
+    file = null;
+  };
 
   // 리빗 창 클릭시 onClick 이벤트 막음.
   MaintainContent = (event) => {
     event.stopPropagation();
+  };
+
+  // 이미지 파일을 불러오기 전에 담겨있는 이미지 파일 초기화 하는 함수
+  ResetFileData = (e) => {
+    e.target.value = null;
   };
 
   render() {
@@ -37,7 +82,26 @@ class RibbitContainer extends Component {
               placeholder="무슨 일이 일어나고 있나요?"
               maxLength="2000"
             />
-            {/* 여기에 그림이미지 */}
+          </div>
+          <div className="RibbitBody__Menu">
+            <label
+              className="far fa-image BodyMenu__Image"
+              id="Image_File_Label"
+              htmlFor="Ribbit__AddImage"
+            >
+              <input
+                style={{ display: 'none' }}
+                type="file"
+                accept="image/*"
+                onClick={e => this.ResetFileData(e)}
+                onChange={e => this.handleFileRibbit(e)}
+                ref="Ribbit__AddImage"
+                id="Ribbit__AddImage"
+              />
+            </label>
+            <div className="BodyMenu__Ribbit">
+              <span className="BodyMenu__Ribbit__Text">리빗하기</span>
+            </div>
           </div>
         </div>
       </div>
