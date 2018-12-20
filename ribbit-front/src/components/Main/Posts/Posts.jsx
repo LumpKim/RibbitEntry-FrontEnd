c0ca37af8483a4a6e233ef989adfbc6a8b10cbb4
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-20 22:52:19 +0900

    ADD : 게시글이 올라갈 장소 컴포넌트 제작

---
+++ ribbit-front/src/components/Main/Posts/Posts.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './css/Posts.css';
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="MainContent__Posts">
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+      </div>
+    );
+  }
+}
+
+export default Posts;
