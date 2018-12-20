f74d513f8e96d27b2ed07a99a13028ca787407a6
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-20 22:52:04 +0900

    ADD : 메인화면에 존재하는 메인색 바꾸는 기능하는 컴포넌트 제작

---
+++ ribbit-front/src/components/Main/ColorSelector/ColorSelector.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/ColorSelector.css';
+
+class ColorSelector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="MainContent__ColorSelctor">안녕하세요3</div>;
+  }
+}
+
+export default ColorSelector;
