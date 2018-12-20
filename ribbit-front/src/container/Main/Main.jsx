03fc1c38558dd327127b98ef35f3af3c7569dc48
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-20 22:51:38 +0900

    ADD : 메인페이지 틀 잡기

--- ribbit-front/src/container/Main/Main.jsx
+++ ribbit-front/src/container/Main/Main.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import './css/Main.css';
+import { Profile, Posts, ColorSelector } from '../../components/Main/index';
 
-const Main = () => <div>a</div>;
+const Main = () => (
+  <div className="Main">
+    <div className="Main__Content">
+      <Profile />
+      <div className="filter" />
+      <Posts />
+      <div className="filter" />
+      <ColorSelector />
+    </div>
+  </div>
+);
 
 export default Main;
