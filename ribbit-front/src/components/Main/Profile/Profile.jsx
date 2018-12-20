d804d3d277cf65680c5d28b42a19658bbee986ee
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-20 22:52:34 +0900

    ADD : 프로필 띄울 컴포넌트 제작

---
+++ ribbit-front/src/components/Main/Profile/Profile.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Profile.css';
+
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="MainContent__Profile">안녕하세요</div>;
+  }
+}
+
+export default Profile;
