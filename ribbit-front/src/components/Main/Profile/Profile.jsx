742abaf91cd07a56d52af9f7bda0a56e2fc3746d
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-21 16:59:33 +0900

    UPDATE : 메인페이지에서 프로필 부분 컴포넌트 완성

--- ribbit-front/src/components/Main/Profile/Profile.jsx
+++ ribbit-front/src/components/Main/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './css/Profile.css';
+import { Link } from 'react-router-dom';
 
 class Profile extends Component {
   constructor(props) {
@@ -8,7 +9,51 @@ class Profile extends Component {
   }
 
   render() {
-    return <div className="MainContent__Profile">안녕하세요</div>;
+    const userName = '김준우';
+    const userAddress = 'WhatTheFuck';
+    const userRibbitData = '숫자1';
+    const userFollowingData = '숫자2';
+    const userFollowersData = '숫자3';
+    return (
+      <div className="MainContent__Profile">
+        <Link to={`/user/${userAddress}`} className="MainProfile__Header">
+          <div className="MainProfile__Header--Background" />
+        </Link>
+        <div className="MainProfile__Body">
+          <Link to={`/user/${userAddress}`} className="ProfileBody__MainPicture">
+            <i className="fas fa-camera BodyMainPicture__CameraIcon" />
+          </Link>
+          <div className="ProfileBody__UserData">
+            <Link to={`/user/${userAddress}`} className="BodyUserData__UserName--Container">
+              <span className="BodyUserData__UserName">{userName}</span>
+            </Link>
+            <Link to={`/user/${userAddress}`} className="BodyUserData__UserID--Container">
+              <span className="BodyUserData__UserID">{`@${userAddress}`}</span>
+            </Link>
+          </div>
+        </div>
+        <div className="MainProfile__Footer">
+          <Link to={`/user/${userAddress}`} className="ProfileFooter__UserData">
+            <div>
+              <p className="FooterUserData__DataIndex">리빗</p>
+              <span className="FooterUserData__Data">{userRibbitData}</span>
+            </div>
+          </Link>
+          <Link to={`/user/${userAddress}/following`} className="ProfileFooter__UserData">
+            <div>
+              <p className="FooterUserData__DataIndex">팔로잉</p>
+              <span className="FooterUserData__Data">{userFollowingData}</span>
+            </div>
+          </Link>
+          <Link to={`/user/${userAddress}/followers`} className="ProfileFooter__UserData">
+            <div>
+              <p className="FooterUserData__DataIndex">팔로워</p>
+              <span className="FooterUserData__Data">{userFollowersData}</span>
+            </div>
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
 
