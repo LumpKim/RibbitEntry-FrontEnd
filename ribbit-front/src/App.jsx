23f384ba151f057d6d157b43527c961a6ade329e
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-21 16:59:06 +0900

    UPDATE : 팔로우, 팔로윙 페이지 Route 설정

--- ribbit-front/src/App.jsx
+++ ribbit-front/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/defaultLayout/Header';
-import { Main, User, Mypage } from './container/index';
+import {
+  Main, User, Mypage, Following, Followers,
+} from './container/index';
 
 const App = () => (
   <div className="App">
@@ -12,14 +14,20 @@ const App = () => (
         {/* Header 컴포넌트와 라우터 컴포넌트가 곂치지 않도록 block역할을 하는 엘리먼트 */}
         <div className="header__background" />
         <Switch>
-          {/* 메인 페이지 */}
           <Route path="/" component={Main} exact />
+          {/* 메인 페이지 */}
 
-          {/* 유저 정보 페이지 */}
           <Route path="/:user" component={User} exact />
+          {/* 유저 정보 페이지 */}
 
-          {/* 마이페이지 */}
           <Route path="/user/:user" component={Mypage} exact />
+          {/* 마이페이지 */}
+
+          <Route path="/user/:user/following" component={Following} exact />
+          {/* 마이페이지 팔로잉 */}
+
+          <Route path="/user/:user/followers" component={Followers} exact />
+          {/* 마이페이지 팔로워 */}
         </Switch>
       </React.Fragment>
     </BrowserRouter>
