65ec0df57480638ac78ae4314d638a088e3dee6a
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-21 17:00:17 +0900

    UPDATE : 팔로잉 팔로워 추가

--- ribbit-front/src/container/index.js
+++ ribbit-front/src/container/index.js
@@ -1,12 +1,18 @@
 import Main from './Main/Main';
 import User from './User/User';
 import Mypage from './MyPage/Mypage';
+import Following from './MyPage/Following/Following';
+import Followers from './MyPage/Followers/Followers';
 
 export {
   // 메인페이지
   Main,
-  // 유저 정보 페이지
+  // 타 유저 정보 페이지
   User,
   // 마이페이지
   Mypage,
+  // 팔로잉
+  Following,
+  // 팔로워
+  Followers,
 };
