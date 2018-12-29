ff889cf6640a8db585ee928a7a6d6cef397097d1
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-29 23:11:53 +0900

    UPDATE : 함수형컴포넌트에서 컴포넌트 형으로 변경

--- ribbit-front/src/App.jsx
+++ ribbit-front/src/App.jsx
@@ -1,40 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/defaultLayout/Header';
 import {
-  Main, User, MainMyPage, Following, Followers, Login,
+  Main, User, MainMyPage, Following, Followers, Login, Signup,
 } from './container/index';
 
-const App = () => (
-  <div className="App">
-    <BrowserRouter>
-      <React.Fragment>
-        <Header />
-        {/* Header 컴포넌트와 라우터 컴포넌트가 곂치지 않도록 block역할을 하는 엘리먼트 */}
-        <div className="header__background" />
-        <Switch>
-          <Route path="/" component={Main} exact />
-          {/* 메인 페이지 */}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Whether: false,
+    };
+  }
 
-          <Route path="/:user" component={User} exact />
-          {/* 유저 정보 페이지 */}
+  HandleWhether = () => {
+    this.setState({
+      Whether: true,
+    });
+  };
 
-          <Route path="/username/:user" component={MainMyPage} exact />
-          {/* 마이페이지 */}
+  render() {
+    const { Whether } = this.state;
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <React.Fragment>
+            <Header Whether={Whether} />
+            {/* Header 컴포넌트와 라우터 컴포넌트가 곂치지 않도록 block역할을 하는 엘리먼트 */}
+            <div className="header__background" />
+            <Switch>
+              <Route
+                path="/"
+                component={() => <Main Whether={Whether} HandleWhether={this.HandleWhether} />}
+                exact
+              />
+              {/* 메인 페이지 */}
 
-          <Route path="/user/:user/following" component={Following} exact />
-          {/* 마이페이지 팔로잉 */}
+              <Route path="/:user" component={User} exact />
+              {/* 유저 정보 페이지 */}
 
-          <Route path="/user/:user/followers" component={Followers} exact />
-          {/* 마이페이지 팔로워 */}
+              <Route path="/username/:user" component={MainMyPage} exact />
+              {/* 마이페이지 */}
 
-          <Route path="/user/login" component={Login} exact />
-          {/* 로그인 페이지 */}
-        </Switch>
-      </React.Fragment>
-    </BrowserRouter>
-  </div>
-);
+              <Route path="/username/:user/following" component={Following} exact />
+              {/* 마이페이지 팔로잉 */}
 
+              <Route path="/username/:user/followers" component={Followers} exact />
+              {/* 마이페이지 팔로워 */}
+
+              <Route path="/user/login" component={Login} exact />
+              {/* 로그인 페이지 */}
+
+              <Route path="/user/signup" component={Signup} exact />
+              {/* 회원가입 페이지 */}
+            </Switch>
+          </React.Fragment>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 export default App;
