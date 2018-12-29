e6b44048b4e150a18bf7b0dbb348bd6abe4fd527
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-29 23:21:12 +0900

    UPDATE : 로그인이 안돼있는 메인 페이지 추가

--- ribbit-front/src/container/Main/Main.jsx
+++ ribbit-front/src/container/Main/Main.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './css/Main.css';
 import { Profile, Posts, ColorSelector } from '../../components/Main/index';
+import Register from '../../components/Regiseter/Register/Register';
 
-const Main = () => (
+const Main = ({ Whether, HandleWhether }) => (
   <div className="Main">
-    <div className="Main__Content">
-      <Profile />
-      <div className="filter" />
-      <Posts />
-      <div className="filter" />
-      <ColorSelector />
-    </div>
+    {Whether ? (
+      <div className="Main__Content">
+        <Profile />
+        <div className="filter" />
+        <Posts />
+        <div className="filter" />
+        <ColorSelector />
+      </div>
+    ) : (
+      <div className="Main__Register">
+        <Register HandleWhether={HandleWhether} />
+      </div>
+    )}
   </div>
 );
 
