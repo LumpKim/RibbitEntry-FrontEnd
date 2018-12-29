7c1cc02fe370fb4baa02e8f8232b67f55c4d442a
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-12-29 23:28:13 +0900

    ADD : 로그인 폼 추가

---
+++ ribbit-front/src/container/Regiseter/Login/Login.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import LoginComponent from '../../../components/Regiseter/Login/Login';
+
+const Login = () => <LoginComponent />;
+
+export default Login;
