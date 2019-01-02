e37950606196677e8599ec4b11421cc97b1afda6
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2019-01-02 23:47:28 +0900

    UPDATE : 더미값 제거

--- ribbit-front/src/components/Main/Posts/Posts.jsx
+++ ribbit-front/src/components/Main/Posts/Posts.jsx
@@ -2,67 +2,10 @@ import React, { Component } from 'react';
 import './css/Posts.css';
 import MainRibbit from './MainRibbit';
 
-class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      post: [
-        //
-      ],
-    };
-  }
-
-  render() {
-    return (
-      <div className="MainContent__Posts">
-        <MainRibbit />
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-        <p>f</p>
-      </div>
-    );
-  }
-}
+const Posts = () => (
+  <div className="MainContent__Posts">
+    <MainRibbit />
+  </div>
+);
 
 export default Posts;
