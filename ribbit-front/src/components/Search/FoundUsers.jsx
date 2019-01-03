a5e168ef89d079a02079cb986a91c6f9da564415
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2019-01-03 23:49:15 +0900

    ADD : 검색 시 상태 컴포넌트

---
+++ ribbit-front/src/components/Search/FoundUsers.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './css/FoundUsers.css';
+
+class FoundUsers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div />
+      </div>
+    );
+  }
+}
+
+export default FoundUsers;
---
+++ ribbit-front/src/components/Search/NotFoundUsers.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/NotFoundUsers.css';
+
+const NotFoundUsers = () => (
+  <div>
+    <div />
+  </div>
+);
+
+export default NotFoundUsers;
