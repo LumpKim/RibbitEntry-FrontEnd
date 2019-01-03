68dabe8492a24938f72b884cb96b036b8cf0f30c
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2019-01-03 23:50:11 +0900

    ADD: 검색 상태를 모아둔 index

---
+++ ribbit-front/src/components/Search/index.js
@@ -0,0 +1,5 @@
+import FoundUsers from './FoundUsers';
+import NotFoundUsers from './NotFoundUsers';
+import SearchComponent from './SearchComponent';
+
+export { FoundUsers, NotFoundUsers, SearchComponent };
