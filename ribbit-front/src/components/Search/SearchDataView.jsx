fc7747264d55f392db2630ce6acc942155e60743
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2019-01-05 23:53:28 +0900

    ADD : 데이터를 띄워줄 View

---
+++ ribbit-front/src/components/Search/SearchDataView.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './css/SearchDataView.css';
+
+const SearchDataView = ({ UserData, index }) => (
+  <div key={index + 1} className="SearchBodyContent__Item">
+    <div className="SearchBodyContentItem__Header">
+      <div className="user__profile">
+        {UserData.profile_image ? (
+          <img className="user__profile--image" src={UserData.profile_image} alt="프로필 사진" />
+        ) : (
+          <i
+            className="fas fa-user user__profile"
+            style={{ color: localStorage.color, opacity: 0.6 }}
+          />
+        )}
+      </div>
+    </div>
+    <div className="SearchBodyContent__NameLabel">
+      <span className="SearchBodyContentNameLabel__Name">{UserData.nickname}</span>
+    </div>
+  </div>
+);
+
+export default SearchDataView;
