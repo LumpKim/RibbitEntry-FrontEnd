e572db84afb49ace051cbdd10c86e75a10e052f4
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2019-01-05 23:54:57 +0900

    UPDATE : 검색 시 데이터 띄우는 경로 지정 및 props 설정

--- ribbit-front/src/components/Search/SearchComponent.jsx
+++ ribbit-front/src/components/Search/SearchComponent.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './css/SearchComponent.css';
-import { NotFoundUsers, FoundUsers } from './index';
+import { SearchDataView, NotSearchDataView } from './index';
 
 class SearchComponent extends Component {
   constructor(props) {
@@ -13,16 +12,26 @@ class SearchComponent extends Component {
   }
 
   render() {
-    const { UserData, TalkTitle } = this.props;
-    console.log(UserData[0]);
+    const { UserData, TalkTitle, UserName } = this.props;
     return (
       <div className="SearchContainer">
         <div className="SearchNavigation">
           <div className="SearchNavigation__UserName">
-            <h1 className="SearchNavigationUserName__Title">{UserData}</h1>
+            <h1 className="SearchNavigationUserName__Title">
+              {UserName ? `${UserName}` : '유저 정보가 없습니다.'}
+            </h1>
           </div>
           <div className="SearchNavigation__TextData">
             <h2 className="SearchNavigationTextData__Title">{TalkTitle}</h2>
+          </div>
+        </div>
+        <div className="SearchBody">
+          <div className="SearchBody__Content">
+            {UserData ? (
+              UserData.map((item, index) => <SearchDataView UserData={item} index={index} />)
+            ) : (
+              <NotSearchDataView />
+            )}
           </div>
         </div>
       </div>
