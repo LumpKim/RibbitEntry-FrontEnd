fd355c2b65f8016a3e0977d994eda673cdcc8916
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2019-01-06 23:49:16 +0900

    UPDATE: functinal component 에서 component형으로 변경

--- ribbit-front/src/container/Main/Main.jsx
+++ ribbit-front/src/container/Main/Main.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './css/Main.css';
 import { Profile, Posts, ColorSelector } from '../../components/Main/index';
 import Register from '../../components/Regiseter/Register/Register';
 
-const Main = ({ Whether }) => (
-  <div className="Main">
-    {Whether ? (
-      <div className="Main__Content">
-        <Profile />
-        <div className="filter" />
-        <Posts />
-        <div className="filter" />
-        <ColorSelector />
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.props.GetUserData();
+  }
+
+  render() {
+    const { Whether } = this.props;
+    return (
+      <div className="Main">
+        {Whether ? (
+          <div className="Main__Content">
+            <Profile />
+            <div className="filter" />
+            <Posts />
+            <div className="filter" />
+            <ColorSelector />
+          </div>
+        ) : (
+          <div className="Main__Register">
+            <Register />
+          </div>
+        )}
       </div>
-    ) : (
-      <div className="Main__Register">
-        <Register />
-      </div>
-    )}
-  </div>
-);
+    );
+  }
+}
 
 export default Main;
