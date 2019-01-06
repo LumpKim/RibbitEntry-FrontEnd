107973c8005fa81b8a303db82cc1b7592bbb4a40
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2019-01-06 23:49:36 +0900

    UPDATE : 데이터가 없을 시 나오는 화면 완성

--- ribbit-front/src/components/Search/NotSearchDataView.jsx
+++ ribbit-front/src/components/Search/NotSearchDataView.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './css/NotSearchDataView.css';
 
-const NotSearchDataView = () => (
-  <div>
-    <div />
+const NotSearchDataView = ({ UserName }) => (
+  <div className="SearchBodyContentNotUserDataForm__Item">
+    <div className="SearchBodyContentNotUserDataFormItem__Header">
+      <span className="SearchBodyContentNotUserDataFormItemHeader__Title">{`${UserName}에 대한 정보를 찾을 수 없습니다`}</span>
+    </div>
+    <div className="SearchBodyContentNotUserDataFormItem__Body">
+      <p className="SearchBodyContentNotUserDataFormItemBody__Contents">
+        입력한 용어에 대한 결과를 찾을 수 없습니다. 용어를 잘못 입력했거나 <br /> 검색 설정이
+        잠재적으로 민감한 콘텐츠로부터 사용자를 보호하도록 설정되
+        <br />어 있을 수 있습니다.
+      </p>
+    </div>
   </div>
 );
 
