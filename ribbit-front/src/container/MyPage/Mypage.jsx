ac2cf66edd067fc6ef8d21333bf053d17d16d1a4
CHAEEUN-LEE <lce4376@gmail.com>
2019-01-08 15:32:45 +0900

    EDIT: MainProfile 컴포넌트는 함수형, Mypage는 클래스 형으로 변경

--- ribbit-front/src/container/MyPage/Mypage.jsx
+++ ribbit-front/src/container/MyPage/Mypage.jsx
@@ -3,29 +3,49 @@ import MainProfile from '../../components/MyPage/Profile/MainProfile';
 import UserInfo from '../../components/MyPage/Profile/UserInfo';
 import './css/MyPage.css';
 
-const Mypage = props => (
-  <div>
-    <MainProfile
-      userAddress={props.userAddress}
-      userFollowingData={props.userFollowingData}
-      userFollowerData={props.userFollowerData}
-      headerImg={props.headerImg}
-      profileImg={props.profileImg}
-      buttonStatus={props.buttonStatus}
-    />
-    <div className="my-page">
-      <div className="user-info">
-        <UserInfo
-          userName={props.userName}
-          userAddress={props.userAddress}
-          userIntroduction={props.userIntroduction}
+class Mypage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonValue: false,
+    };
+  }
+
+  render() {
+    const {
+      userAddress,
+      userFollowingData,
+      userFollowerData,
+      headerImg,
+      profileImg,
+      buttonStatus,
+      userName,
+      userIntroduction,
+    } = this.props;
+
+    return (
+      <div>
+        <MainProfile
+          userAddress={userAddress}
+          userFollowingData={userFollowingData}
+          userFollowerData={userFollowerData}
+          headerImg={headerImg}
+          profileImg={profileImg}
+          buttonStatus={buttonStatus}
         />
+        <div className="my-page">
+          <div className="user-info">
+            <UserInfo
+              userName={userName}
+              userAddress={userAddress}
+              userIntroduction={userIntroduction}
+            />
+          </div>
+          <div className="page-content">fff</div>
+        </div>
       </div>
-      <div className="page-content">
-        fff
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Mypage;
