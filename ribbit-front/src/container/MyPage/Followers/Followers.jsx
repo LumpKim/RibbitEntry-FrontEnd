ff91785a9c8ca6ffa104da41a3c43122a389e2fd
CHAEEUN-LEE <lce4376@gmail.com>
2019-01-08 09:29:56 +0900

    ADD: UserInfo에 nickname과 userId 값 전달

--- ribbit-front/src/container/MyPage/Followers/Followers.jsx
+++ ribbit-front/src/container/MyPage/Followers/Followers.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MainProfile from '../../../components/MyPage/Profile/MainProfile';
 import Follow from '../Follow';
 import UserInfo from '../../../components/MyPage/Profile/UserInfo';
 import '../css/MyPage.css';
 
-const Followers = () => (
-  <div>
-    <div>
-      <MainProfile />
-    </div>
-    <div className="my-page">
-      <div className="user-info">
-        <UserInfo />
+class Followers extends Component {
+  render() {
+    return (
+      <div>
+        <MainProfile />
+        <div className="my-page">
+          <div className="user-info">
+            <UserInfo
+              userName={this.props.userName}
+              userAddress={this.props.userAddress}
+            />
+          </div>
+          <div className="page-content">
+            <Follow />
+          </div>
+        </div>
       </div>
-      <div className="page-content">
-        <Follow />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Followers;
