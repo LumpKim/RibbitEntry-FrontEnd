e896a207924b8ad166037476bc79f5d63ad09124
CHAEEUN-LEE <lce4376@gmail.com>
2019-01-08 09:23:49 +0900

    ADD: UserInfo에 nickname과 userId 값 전달

--- ribbit-front/src/container/MyPage/Follow.jsx
+++ ribbit-front/src/container/MyPage/Follow.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/Follow.css';
 import { Link } from 'react-router-dom';
 import { RibbitIcon } from '../../assets/index';
 
-const userName = '리비터';
-const userAddress = 'otherribbiter';
-const followingState = 'following';
+class Follow extends Component {
+  render() {
+    const userName = 'follower';
+    const userAddress = 'following';
 
-const Follow = () => (
-  <div className="follow__profile">
-    <Link to={`/user/${userAddress}`}>
-      <div className="follow__profile-cast">
-        <div>
-          <img src={RibbitIcon} alt="Profile" className="follow__profile-photo" />
-        </div>
-        <div className="follow__user-info">
-          <span className="user-name">{userName}</span>
-          <br />
-          <span className="user-address">
-            @
-            {userAddress}
-          </span>
-        </div>
+    return (
+      <div className="follow__profile">
+        <Link to={`/user/${userAddress}`}>
+          <div className="follow__profile-cast">
+            <div>
+              <img src={RibbitIcon} alt="Profile" className="follow__profile-photo" />
+            </div>
+            <div className="follow__user-info">
+              <span className="user-name">{userName}</span>
+              <br />
+              <span className="user-address">
+                @
+                {userAddress}
+              </span>
+            </div>
+          </div>
+        </Link>
       </div>
-    </Link>
-    <button type="button" className="following-button">
-      {followingState}
-    </button>
-  </div>
-);
+    );
+  }
+}
 
 export default Follow;
