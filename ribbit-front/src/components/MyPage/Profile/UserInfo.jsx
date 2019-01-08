2273a0e7cb8eddb64ca1b38a3429f14308646ffa
CHAEEUN-LEE <lce4376@gmail.com>
2019-01-08 09:04:43 +0900

    ADD: userName, userAddress 값 서버에서 받아옴

--- ribbit-front/src/components/MyPage/Profile/UserInfo.jsx
+++ ribbit-front/src/components/MyPage/Profile/UserInfo.jsx
@@ -1,33 +1,17 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './css/UserInfo.css';
 
 class UserInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userAddress: 'defaultAddress',
-      userName: '리비터',
-    };
-  }
-
-  componentDidMount() {
-    axios.get(`http://ribbit.jaehoon.kim:5000/api/${userAddress}/profile`).then(res => {
-      const nickname = res.data.nickname;
-      this.setState({ userName });
-    });
-  }
-
   render() {
     return (
       <div className="user-info">
-        <Link to={`/user/${this.state.userAddress}`} className="user-name-id">
-          <span className="user-name">{this.state.userName}</span>
+        <Link to={`/user/${this.props.userAddress}`} className="user-name-id">
+          <span className="user-name">{this.props.userName}</span>
           <br />
           <span className="user-address">
             @
-            {this.state.userAddress}
+            {this.props.userAddress}
           </span>
         </Link>
       </div>
