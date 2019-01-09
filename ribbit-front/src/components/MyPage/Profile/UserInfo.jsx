7e4eea65f438aec0665974ad8a98d8897511e47e
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2019-01-09 13:27:36 +0900

    UPDATE : 유저 데이터 열람

--- ribbit-front/src/components/MyPage/Profile/UserInfo.jsx
+++ ribbit-front/src/components/MyPage/Profile/UserInfo.jsx
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './css/UserInfo.css';
 
-const UserInfo = ({ userAddress, userName, userIntroduction }) => (
+const UserInfo = ({
+  userAddress, userName, userIntroduction, CampareUser,
+}) => (
   <div className="user-info">
-    <Link to={`/username/${userAddress}`} className="user-name-id">
+    <Link
+      to={CampareUser ? `/${userAddress}` : `/username/${userAddress}`}
+      className="user-name-id"
+    >
       <span className="user-name">{userName}</span>
       <br />
       <span className="user-address">@{userAddress}</span>
