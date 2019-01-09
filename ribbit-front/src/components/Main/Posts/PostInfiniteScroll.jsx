fdb7f6ecc2421ff40b276d4b8858677555a7468c
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2019-01-09 10:18:31 +0900

    UPDATE : 게시글 컴포넌트 분리

--- ribbit-front/src/components/Main/Posts/PostInfiniteScroll.jsx
+++ ribbit-front/src/components/Main/Posts/PostInfiniteScroll.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { Loading, RibbitOn, RibbitOff } from '../../../assets/index';
+import { Loading } from '../../../assets/index';
+import PostInfiniteItem from './PostInfiniteItem';
 import './css/PostInfiniteScroll.css';
 
 class PostInfiniteScroll extends Component {
@@ -15,82 +14,31 @@ class PostInfiniteScroll extends Component {
   }
 
   componentDidMount() {
-    if (this.props.post) {
+    if (!this.props.post) {
       this.setState({
         hasMoreItems: false,
       });
     }
   }
-  // ToggleItems = () => {
-
-  // };
 
   render() {
-    const { post, RibbitLikeToggle, RibbitLike } = this.props;
+    const { post, GetUserData } = this.props;
     const loader = (
       <div key={0} className="loader">
         <img src={Loading} alt="로딩" width="35px" height="35px" />
       </div>
     );
 
-    const items = [];
-
-    if (post) {
-      post.map((track, i) => items.push(
-        <div className="track">
-          <div className="track__imageHolder">
-            <Link to={`/${track.user}`} className="track__profile">
-              {track.proimg ? (
-                <img src={track.proimg} alt="프로필사진" className="user__profileImage" />
-              ) : (
-                <i
-                  className="fas fa-user user__profile"
-                  style={{ color: localStorage.color, opacity: 0.6 }}
-                />
-              )}
-            </Link>
-          </div>
-          <div className="track__bodyHolder">
-            <div className="track__body--header">
-              <Link to={`/${track.user}`} className="track__userLink">
-                <span className="track__userName">{track.nickname}</span>
-                <span className="track__userId">@{track.user}</span>
-              </Link>
-              <span className="track__date">{track.date}</span>
-            </div>
-            <div className="track__body--content">
-              <p className="track__contents">{track.content}</p>
-            </div>
-            <div className="track__body--footer">
-              <div className="track__body--footer__Item">
-                <div className="track__footer--action track--comment">
-                  <i className="far fa-comment-alt track--icon" />
-                </div>
-                <div
-                  className="track__footer--action track--favorite"
-                  onClick={() => RibbitLikeToggle(track.id)}
-                >
-                  {RibbitLike ? (
-                    <img src={RibbitOn} alt="리빗On" className="track--icon" />
-                  ) : (
-                    <img src={RibbitOff} alt="리빗Off" className="track--icon" />
-                  )}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>,
-      ));
-    }
+    const items = post && post.map(da => <PostInfiniteItem track={da} GetUserData={GetUserData} />);
 
     return (
       <div className="InfiniteScrollContainer">
         <InfiniteScroll hasMore={this.state.hasMoreItems} loader={loader}>
           <div className="InfiniteScrollContainer__Item">
-            {post !== [] ? (
-              <span className="InfiniteScrollNotItem">팔로우 / 팔로윙 데이터가 없습니다.</span>
-            ) : (
+            {post !== null ? (
               items
+            ) : (
+              <span className="InfiniteScrollNotItem">팔로우 / 팔로윙 데이터가 없습니다.</span>
             )}
           </div>
         </InfiniteScroll>
